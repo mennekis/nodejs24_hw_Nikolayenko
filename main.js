90e9417a40713de98a06a6b89c3d756dae2d1ad9
Oleksiy Nikolayenko <mennekis@gmail.com>
2024-01-28 12:25:41 +0200

    Home2 started

--- main.js
+++ main.js
@@ -5,3 +5,4 @@ const { auth } = require("./auth");
 
 logger.info("working OK", "test arg", dbId, `auth status:${auth}`);
 logger.warn({ contextId: 42, title: "something wrong might happen" });
+logger.error("testing error");
--- utils/logger.js
+++ utils/logger.js
@@ -1,8 +1,9 @@
 function logger(initialName) {
+   const colors = require("colors");
    return {
-      info: (...args) => console.info(initialName, ...args),
-      warn: (...args) => console.warn(initialName, ...args),
-      error: (...args) => console.error(initialName, ...args),
+      info: (...args) => console.info("#_ " + initialName.blue, ...args),
+      warn: (...args) => console.warn("*_ " + initialName.yellow, ...args),
+      error: (...args) => console.error("!_ " + initialName.red, ...args),
    };
 }
 
