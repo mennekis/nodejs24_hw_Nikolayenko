32b6499413d3a20693a3d4bd86fbb9bd3c4de463
Oleksiy Nikolayenko <mennekis@gmail.com>
2024-02-01 21:38:09 +0200

    make some optimisation

--- utils/logger.js
+++ utils/logger.js
@@ -1,23 +1,23 @@
-const colorMode = process.env.COLORS_ENABLED === "1" ? "1" : "0";
+const colorMode = process.env.COLORS_ENABLED === "1";
 const logLevel = process.env.NODE_ENV === "prod" ? "error" : "info";
 colors = require("colors/safe");
 
 function logger(initialName) {
    console.log(colorMode, logLevel);
-   if (colorMode === "1") {
+   if (colorMode) {
       return {
          info: (...args) =>
-            console.info(colors.bgGreen(initialName + ":"), ...args),
+            console.info(colors.bgGreen(`${initialName}:`), ...args),
          warn: (...args) =>
-            console.warn(colors.bgYellow(initialName + ":"), ...args),
+            console.warn(colors.bgYellow(`${initialName}:`), ...args),
          error: (...args) =>
-            console.error(colors.bgRed(initialName + ":"), ...args),
+            console.error(colors.bgRed(`${initialName}:`), ...args),
       };
    } else {
       return {
-         info: (...args) => console.info(initialName + ":", ...args),
-         warn: (...args) => console.warn(initialName + ":", ...args),
-         error: (...args) => console.error(initialName + ":", ...args),
+         info: (...args) => console.info(`${initialName}:`, ...args),
+         warn: (...args) => console.warn(`${initialName}:`, ...args),
+         error: (...args) => console.error(`${initialName}:`, ...args),
       };
    }
 }
