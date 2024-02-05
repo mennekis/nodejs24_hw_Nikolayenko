c06ed069a7400dc265063e0dcff0956e37b1d909
Oleksiy Nikolayenko <mennekis@gmail.com>
2024-02-05 22:00:26 +0200

    In Process Homework_3

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 require("dotenv").config();
 
 const config = require("config");
+
+const fileSync = require("./file_sync");
 console.log(config);
 
 const logger = require("./utils/logger")("main");
 
-const { dbId } = require("./db");
-const { auth } = require("./auth");
+fileSync.start();
+// const { dbId } = require("./db");
+// const { auth } = require("./auth");
 
-logger.info("working OK", "test arg", dbId, `auth status:${auth}`);
-logger.warn({ contextId: 42, title: "something wrong might happen" });
-logger.error("testing error");
-logger.info("The script is running");
+// logger.info("working OK", "test arg", dbId, `auth status:${auth}`);
+// logger.warn({ contextId: 42, title: "something wrong might happen" });
+// logger.error("testing error");
+// logger.info("The script is running");
