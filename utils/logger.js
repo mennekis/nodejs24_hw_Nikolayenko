d128b0801f397d548e3be2f95dd24c023f4288f2
Oleksiy Nikolayenko <mennekis@gmail.com>
2024-02-09 22:10:05 +0200

    hometask_4

--- utils/logger.js
+++ utils/logger.js
@@ -1,30 +1,67 @@
-const colors = require('colors/safe');
-const { logLevel, colorMode } = require('config');
+const fs = require("fs");
+const path = require("path");
+const colors = require("colors/safe");
+const { logLevel, colorMode } = require("config");
+
+function getCurrentDateTime() {
+   return new Date().toISOString();
+}
 
 function logger(initialName) {
-  console.log(colorMode, logLevel);
+   console.log(colorMode, logLevel);
 
-  /* 1. на етапі ініціалізації логгера, один раз перевіряємо чи включені у нас кольори в конфігу.
-   Якщо виключені - виключаємо цей фунцкіонал в коді. Достатньо це зробити саме тут
-   і більше не чипати методи пакета `colors`
-  */
-  if (colorMode == '0') {
-    colors.disable(); // по дефолту в режимі `/safe` кольори активні
-  }
+   const logsDir = path.join(__dirname, "logs");
+   try {
+      if (!fs.existsSync(logsDir)) {
+         fs.mkdirSync(logsDir);
+      }
+   } catch (error) {
+      console.error(`Error creating logs directory: ${error.message}`);
+      throw error;
+   }
+   const infoStream = fs.createWriteStream(path.join(logsDir, "info.log"), {
+      flags: "a",
+   });
+   const errorStream = fs.createWriteStream(path.join(logsDir, "errors.log"), {
+      flags: "a",
+   });
 
-  return {
-    info: (...args) => {
-      if (logLevel === 'info') {
-        console.info(colors.bgGreen(`${initialName}:`), ...args);
-      }
-    },
-    warn: (...args) => {
-      if (logLevel !== 'error') {
-        console.warn(colors.bgYellow(`${initialName}:`), ...args);
-      }
-    },
-    error: (...args) => console.error(colors.bgRed(`${initialName}:`), ...args),
-  };
+   process.on("beforeExit", () => {
+      infoStream.end();
+      errorStream.end();
+   });
+
+   if (colorMode == "0") {
+      colors.disable();
+   }
+
+   return {
+      info: (...args) => {
+         if (logLevel === "info") {
+            const logMessage = `${getCurrentDateTime()} [INFO] ${initialName}: ${args.join(
+               " "
+            )}\n`;
+            console.info(colors.bgGreen(`${initialName}:`), ...args);
+            infoStream.write(logMessage);
+         }
+      },
+      warn: (...args) => {
+         if (logLevel !== "error") {
+            const logMessage = `${getCurrentDateTime()} [WARN] ${initialName}: ${args
+               .map(JSON.stringify)
+               .join(" ")}\n`;
+            console.warn(colors.bgYellow(`${initialName}:`), ...args);
+            errorStream.write(logMessage);
+         }
+      },
+      error: (...args) => {
+         const logMessage = `${getCurrentDateTime()} [ERROR] ${initialName}: ${args.join(
+            " "
+         )}\n`;
+         console.error(colors.bgRed(`${initialName}:`), ...args);
+         errorStream.write(logMessage);
+      },
+   };
 }
 
 module.exports = logger;
